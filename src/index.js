cb94fdb939fd16d431a0df238e59a1d159934059
John Phonxaya <phonxayajohn@gmail.com>
2023-05-31 12:31:50 -0500

    practice code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+const ramenAPI = `http://localhost:3000/ramens`
+const ramenMenuElement = document.getElementById("ramen-menu")
+let ramenList = []
+
+fetch(ramenAPI)
+    .then(res => res.json())
+    .then(renderRamens)
+
+function renderRamens(ramens) {
+    ramens.forEach(renderRamen);
+}
+
+function renderRamen(ramen) {
+    const imgElement = document.createElement('img')
+    imgElement.classList.add('ramen-img')
+    imgElement.src = `${ramen.image}`
+    imgElement.alt = `${ramen.name} from ${ramen.restaurant}`
+
+    ramenMenuElement.append(imgElement)
+}
+
+//create button from image and display on #ramen-details
